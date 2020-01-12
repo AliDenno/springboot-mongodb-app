eaca1cd21d49e40804eec09f6cf77fd61d3ae8ee
Ali Denno <aliedenno@gmail.com>
2020-01-12 22:35:18 +0100

    Add ProductReview

---
+++ src/main/java/org/xrds/store/model/ProductReview.java
@@ -0,0 +1,21 @@
+package org.xrds.store.model;
+import org.springframework.data.mongodb.core.index.TextIndexed;
+
+public class ProductReview {
+    @TextIndexed
+    private String userName;
+    private int rating;
+
+    public ProductReview(String userName, int rating) {
+        this.userName = userName;
+        this.rating = rating;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+}
