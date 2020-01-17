f709de6160bf8e8f393759b901fc5866933e351e
Ali Denno <aliedenno@gmail.com>
2020-01-17 17:44:36 +0100

    Update LegoStoreController
    Add more queries, ex. get records by theme or by id

--- src/main/java/org/xrds/store/api/LegoStoreController.java
+++ src/main/java/org/xrds/store/api/LegoStoreController.java
@@ -1,10 +1,13 @@
 package org.xrds.store.api;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.web.bind.annotation.*;
 import org.xrds.store.model.LegoSet;
 import org.xrds.store.model.LegoSetDifficulty;
+import org.xrds.store.model.QLegoSet;
 import org.xrds.store.persistence.LegoSetRepository;
 
 import java.util.Collection;
@@ -32,4 +35,29 @@ public class LegoStoreController {
     public void delete(@PathVariable String id){
         this.legoSetRepository.deleteById(id);
     }
+
+    @GetMapping("/all")
+    public Collection<LegoSet> all(){
+        Sort sortByThemeAsc = Sort.by("theme").ascending();
+        Collection<LegoSet> legosets = this.legoSetRepository.findAll(sortByThemeAsc);
+        return legosets;
+    }
+
+    @GetMapping("/{id}")
+    public LegoSet byId(@PathVariable String id){
+        LegoSet legoSet = this.legoSetRepository.findById(id).orElse(null);
+        return legoSet;
+    }
+
+    @GetMapping("/byTheme/{theme}")
+    public Collection<LegoSet> byTheme(@PathVariable String theme){
+        Sort sortByThemeAsc = Sort.by("theme").ascending();
+        return this.legoSetRepository.findAllByThemeContains(theme, sortByThemeAsc);
+    }
+
+    @GetMapping("hardThatStartWithM")
+    public Collection<LegoSet> hardThatStartWithM(){
+        return this.legoSetRepository.findAllByDifficultyAndNameStartsWith(LegoSetDifficulty.HARD, "M");
+    }
+
 }
