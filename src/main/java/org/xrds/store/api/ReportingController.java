ec06ad4d565f4a83416b26719f2795b942277a23
Ali Denno <aliedenno@gmail.com>
2020-01-21 22:11:24 +0100

    Add ReportingController

---
+++ src/main/java/org/xrds/store/api/ReportingController.java
@@ -0,0 +1,25 @@
+package org.xrds.store.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.xrds.store.model.AvgRatingModel;
+import org.xrds.store.persistence.ReportService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("legostore/api/reports")
+public class ReportingController {
+    private ReportService reportService;
+
+    public ReportingController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @GetMapping("avgRatingsReport")
+    public List<AvgRatingModel> avgRatingReport(){
+        return this.reportService.getAvgRatingReport();
+    }
+}
